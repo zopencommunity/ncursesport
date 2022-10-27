f5e81c1ca98ef4b5adadba4c87a00df9750934f6
Igor Todorovski <itodorov@ca.ibm.com>
2022-10-27 14:21:50 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+ncurses
+
+terminal emulation
---
+++ cicd.groovy
@@ -0,0 +1,6 @@
+node('linux')
+{
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/ncursesport.git'), string(name: 'PORT_DESCRIPTION', value: 'terminal emulation' )]
+  }
+}
