4c2bd7eac597182cf5837d1e71379ddf14c796ae
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:20:27 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -7,9 +7,9 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/ncursesport.git"]]])
+                        userRemoteConfigs: [[url: "https://github.com/zopencommunity/ncursesport.git"]]])
         }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/ncursesport.git'), string(name: 'PORT_DESCRIPTION', value: 'terminal emulation' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/ncursesport.git'), string(name: 'PORT_DESCRIPTION', value: 'terminal emulation' )]
   }
 }
